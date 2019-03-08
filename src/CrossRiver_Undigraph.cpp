25728386491dc5330e0e3f6814fa427f5c7b578f
Viviyogurt <emilyyogurt@163.com>
2019-03-08 17:40:54 +0800

    add new cpp

---
+++ src/CrossRiver_Undigraph.cpp
@@ -0,0 +1,78 @@
+﻿#include <cstdio>
+#include<iostream>
+#define maxn 100
+
+using namespace std;
+int num;
+int grid[maxn*maxn][maxn*maxn];
+int state[maxn][maxn];
+
+int cmer[5] = { 2, 1, 0, 1, 0 };
+int cser[5] = { 0, 1, 2, 0, 1 };
+int mstep[maxn*maxn];
+int sstep[maxn*maxn];
+
+
+bool flag = false;
+void DFS(int mer, int ser, int step, int dir)
+{
+	mstep[step] = mer, sstep[step] = ser;
+	if (mer == 0 && ser == 0)
+	{
+		for (int i = 0; i <= step; i++)
+		{
+			printf("(%d,%d)", mstep[i], sstep[i]);
+			if (i != step)
+				printf(" -> ");
+		}
+		printf("\n");
+		flag = true;
+	}
+	int before = mer * (num + 1) + ser;
+	for (int i = 0; i < 5; i++)
+	{
+		if (dir) {
+			int mnext = mer - cmer[i], snext = ser - cser[i];
+			if (mnext >= 0 && mnext < num + 1 && snext >= 0 && snext < num + 1 && state[mnext][snext])
+			{
+				int after = mnext * (num + 1) + snext;
+				if (!grid[before][after] && !grid[after][before])
+				{
+					grid[before][after] = 1;
+					grid[after][before] = 1;
+					DFS(mnext, snext, step + 1, !dir);
+					grid[before][after] = 0;
+					grid[before][after] = 0;
+				}
+			}
+		}
+		else {
+			int mnext = mer + cmer[i], snext = ser + cser[i];
+			if (mnext >= 0 && mnext < num + 1 && snext >= 0 && snext < num + 1 && state[mnext][snext])
+			{
+				int after = mnext * (num + 1) + snext;
+				if (!grid[before][after] && !grid[after][before])
+				{
+					grid[before][after] = 1;
+					grid[after][before] = 1;
+					DFS(mnext, snext, step + 1, !dir);
+					grid[before][after] = 0;
+					grid[before][after] = 0;
+				}
+			}
+		}
+	}
+}
+int main()
+{
+	cin >> num;
+	for (int i = 0; i < num + 1; i++)
+	{
+		state[i][0] = 1;
+		state[i][num] = 1;
+		state[i][i] = 1;
+	}
+	DFS(num, num, 0, 1);
+	if (!flag)
+		printf("they can't cross the river.");
+}
